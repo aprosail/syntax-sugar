0d15232a002b697eb7d2d2a5960d4e3399e9dd8d
James Aprosail <aprosail@outlook.com>
2024-05-07 12:17:05 +0800

    template rm dart codes

--- example/syntax_sugar_example.dart
+++ example/syntax_sugar_example.dart
@@ -1,6 +1,3 @@
-import 'package:syntax_sugar/syntax_sugar.dart';
-
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  print('it works');
 }
--- test/syntax_sugar_test.dart
+++ test/syntax_sugar_test.dart
@@ -1,16 +1,5 @@
-import 'package:syntax_sugar/syntax_sugar.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
+  test('placeholder', () => expect(1 + 1, 2));
 }
