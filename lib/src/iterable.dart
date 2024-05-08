e9dec5cc60daf3dfaf01c38ddc0704c88e0fca09
James Aprosail <aprosail@outlook.com>
2024-05-08 21:52:20 +0800

    doc iterable performance

--- lib/src/iterable.dart
+++ lib/src/iterable.dart
@@ -1,7 +1,11 @@
 extension AllToString<T> on Iterable<T> {
+  /// Try to use more [toStringList] if possible for better performance.
+  /// This method will also call such method exactly.
   Set<String> toStringSet() => toStringList().toSet();
 
+  /// Attention that the string list is not growable.
   List<String> toStringList() {
+    // Disable growable to make the list linear to improve performance.
     final linearList = toList(growable: false);
     return List.generate(length, (i) => linearList[i].toString());
   }
