be75ef7313b430413c37da0490adc79777478b9e
James Aprosail <aprosail@outlook.com>
2024-05-08 21:37:57 +0800

    regexp chain and matrix

--- lib/src/iterable.dart
+++ lib/src/iterable.dart
@@ -6,3 +6,13 @@ extension AllToString<T> on Iterable<T> {
     return List.generate(length, (i) => linearList[i].toString());
   }
 }
+
+extension RegexpMatchOneOf on Iterable<String> {
+  /// Generate a regular expression that
+  /// matches any of the strings in the iterable.
+  RegExp get regexp {
+    final linearList = toList(growable: false);
+    final escaped = List.generate(length, (i) => RegExp.escape(linearList[i]));
+    return RegExp('(${escaped.join('|')}})');
+  }
+}
---
+++ lib/src/regexp.dart
@@ -0,0 +1,21 @@
+import 'iterable.dart';
+
+/// Chain style programming syntax sugar for regular expressions on strings.
+/// This extension directly mapped the [RegExp] interface in the `dart:core`.
+extension RegexpMatchChain on String {
+  bool hasMatch(RegExp regexp) => regexp.hasMatch(this);
+
+  RegExpMatch? matchFirst(RegExp regexp) => regexp.firstMatch(this);
+
+  Iterable<RegExpMatch> matchAll(RegExp regexp, [int start = 0]) =>
+      regexp.allMatches(this, start);
+}
+
+extension MatchOneOf on String {
+  /// Match the first of any of the strings in the [items] in this string.
+  RegExpMatch? matchFirstOf(Iterable<String> items) => matchFirst(items.regexp);
+
+  /// Match all of any of the strings in the [items] in this string.
+  Iterable<RegExpMatch> matchOneOf(Iterable<String> items) =>
+      matchAll(items.regexp);
+}
