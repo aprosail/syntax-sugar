2040fae86abecb9f921993f1f1a4f37c77312958
James Aprosail <aprosail@outlook.com>
2024-05-08 17:05:32 +0800

    optimize simplify conditions

--- lib/src/nullable.dart
+++ lib/src/nullable.dart
@@ -2,16 +2,14 @@ import 'dart:math';
 
 T? nullableMin<T extends num>(T? a, T? b) {
   if (a == null && b == null) return null;
-  if (a != null && b == null) return a;
-  if (a == null && b != null) return b;
-  assert(a != null && b != null, '$a and $b must all not null here');
-  return min(a!, b!);
+  if (a == null) return b;
+  if (b == null) return a;
+  return min(a, b);
 }
 
 T? nullableMax<T extends num>(T? a, T? b) {
   if (a == null && b == null) return null;
-  if (a != null && b == null) return a;
-  if (a == null && b != null) return b;
-  assert(a != null && b != null, '$a and $b must all not null here');
-  return max(a!, b!);
+  if (a == null) return b;
+  if (b == null) return a;
+  return max(a, b);
 }
